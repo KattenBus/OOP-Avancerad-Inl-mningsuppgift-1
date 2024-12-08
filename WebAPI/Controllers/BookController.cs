2fbbd8b72b35a92e72daf0423c32439f94172e95
KattenBus <simon.oman.rinne@gmail.com>
2024-12-08 20:53:56 +0100

    Added Authorize back To GetAllBooks in BookController

--- WebAPI/Controllers/BookController.cs
+++ WebAPI/Controllers/BookController.cs
@@ -6,6 +6,7 @@ using Application.Books.Queries.GetBookById;
 using Application.Dtos;
 using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +27,7 @@ namespace WebAPI.Controllers
         }
 
         // GET: api/<BookController>
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<BookDto>>> Get()
         {
